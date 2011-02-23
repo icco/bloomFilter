84677ddd416f2a5295bdae902d138a685b8b23cb
Nat Welch <nat@natwelch.com>
2011-02-23 09:36:54 -0800

    Rails init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloomflower::Application.config.secret_token = 'af43b613a47b5270a8f03d7c5d6078f8eb13ba3e14085cd724d7105f93d682151c44640aa728b7c25aca1de28e1aa41f28da7309fa1e4a4d4274aee50e986acf'
