4cc3cbfbf295fd0134ce3cd4aceccc1fd6934fa4
Nat Welch <nat@natwelch.com>
2011-02-28 13:39:48 -0800

    comments on the models.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
+# The item class is a user post. It defines who posted it, when it was posted,
+# what data it contains and if it is a child of something.
 class Item < ActiveRecord::Base
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
+# A user. They have a unique userid and some data about them
 class User < ActiveRecord::Base
 end
