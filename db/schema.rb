7941d438059f329a43493b5de03373ec67930c73
Nat Welch <nat@natwelch.com>
2011-03-01 11:38:23 -0800

    update schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110228234648) do
+ActiveRecord::Schema.define(:version => 20110301193219) do
 
   create_table "items", :force => true do |t|
     t.string   "url"
