857cdb74bc42c0e54264cd3b886dd95be69b9ec6
Nat Welch <nat@natwelch.com>
2011-03-30 13:19:56 -0700

    Small tweaks to app.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,85 +1,71 @@
 class ItemsController < ApplicationController
-  # GET /items
-  # GET /items.xml
-  def index
-    @items = Item.all
+   # GET /items
+   def index
+      @items = Item.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @items }
-    end
-  end
-
-  # GET /items/1
-  # GET /items/1.xml
-  def show
-    @item = Item.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @item }
-    end
-  end
-
-  # GET /items/new
-  # GET /items/new.xml
-  def new
-    @item = Item.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @item }
-    end
-  end
-
-  # GET /items/1/edit
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  # POST /items
-  # POST /items.xml
-  def create
-    @item = Item.new(params[:item])
-
-    @item.user = current_user
-
-    respond_to do |format|
-      if @item.save
-        format.html { redirect_to(@item, :notice => 'Item was successfully created.') }
-        format.xml  { render :xml => @item, :status => :created, :location => @item }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @item.errors, :status => :unprocessable_entity }
+      respond_to do |format|
+         format.html # index.html.erb
       end
-    end
-  end
+   end
 
-  # PUT /items/1
-  # PUT /items/1.xml
-  def update
-    @item = Item.find(params[:id])
+   # GET /items/1
+   def show
+      @item = Item.find(params[:id])
 
-    respond_to do |format|
-      if @item.update_attributes(params[:item])
-        format.html { redirect_to(@item, :notice => 'Item was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @item.errors, :status => :unprocessable_entity }
+      respond_to do |format|
+         format.html # show.html.erb
       end
-    end
-  end
+   end
 
-  # DELETE /items/1
-  # DELETE /items/1.xml
-  def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
+   # GET /items/new
+   def new
+      @item = Item.new
 
-    respond_to do |format|
-      format.html { redirect_to(items_url) }
-      format.xml  { head :ok }
-    end
-  end
+      respond_to do |format|
+         format.html # new.html.erb
+      end
+   end
+
+   # GET /items/1/edit
+   def edit
+      @item = Item.find(params[:id])
+   end
+
+   # POST /items
+   def create
+      @item = Item.new(params[:item])
+
+      @item.user = current_user
+
+      respond_to do |format|
+         if @item.save
+            format.html { redirect_to(@item, :notice => 'Item was successfully created.') }
+         else
+            format.html { render :action => "new" }
+         end
+      end
+   end
+
+   # PUT /items/1
+   def update
+      @item = Item.find(params[:id])
+
+      respond_to do |format|
+         if @item.update_attributes(params[:item])
+            format.html { redirect_to(@item, :notice => 'Item was successfully updated.') }
+         else
+            format.html { render :action => "edit" }
+         end
+      end
+   end
+
+   # DELETE /items/1
+   def destroy
+      @item = Item.find(params[:id])
+      @item.destroy
+
+      respond_to do |format|
+         format.html { redirect_to(items_url) }
+      end
+   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ActiveRecord::Base
    belongs_to :user
    has_many :items
+   has_many :votes
 end
