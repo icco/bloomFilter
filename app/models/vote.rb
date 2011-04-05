bf3276823b9843d02043ea177d082992f49f2caa
Nat Welch <nat@natwelch.com>
2011-04-05 12:34:31 -0700

    Remove depricated validate call.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,7 @@ class Vote < ActiveRecord::Base
    belongs_to :user
    belongs_to :item
 
-   validate_on_create :one_vote
+   validate :one_vote, :on => :create
 
    def one_vote
       errors.add_to_base("This vote already exists") if Vote.where({:item_id => self.item, :user_id => self.user}).count > 0
