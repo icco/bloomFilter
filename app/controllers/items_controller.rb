7a53f2d014d15cb3a7bacffd4306d753a561c360
Nat Welch <nat@natwelch.com>
2011-06-08 12:23:37 -0700

    Bug from when I ripped out comments.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,11 +41,7 @@ class ItemsController < ApplicationController
 
       respond_to do |format|
          if @item.save
-            if @item.is_root?
-               format.html { redirect_to(@item, :notice => 'Item was successfully created.') }
-            else
-               format.html { redirect_to(@item.parent, :notice => 'Comment was successfully created.') }
-            end
+            format.html { redirect_to(@item, :notice => 'Item was successfully created.') }
          else
             format.html { render :action => "new" }
          end
